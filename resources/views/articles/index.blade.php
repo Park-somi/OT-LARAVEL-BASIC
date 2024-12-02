afa86bc0db0c50fc6c3ef7da8e65011dc91f5b6b
Park-somi <somp8320@gmail.com>
2024-12-02 14:24:54 +0900

    [박소미] 31. 블레이드 컴포넌트를 이용한 레이아웃

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,34 +1,33 @@
-<!DOCTYPE html>
-    <head>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div class="container p-5">
-            <h1 class="text-2xl mb-5">글목록</h1>
-            @foreach($articles as $article)
-                <div class="background-white border rounded mb-3 p-3">
-                    <p>{{ $article->body }}</p>
-                    <p>{{ $article->user->name }}</p>
-                    <p><a href="{{ route('articles.show', ['article' => $article->id]) }}">{{ $article->created_at->diffForHumans() }}</p>
-                    <div class="flex flex-row">
-                        <p class="mr-1">
-                            <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="button rounded bg-blue-500 px-2 py-1 text-xs text-white">
-                                수정
-                            </a>
-                        </p>
-                        <form action="{{ route('articles.delete', ['article' => $article->id]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="py-1 px-2 bg-red-500 text-white rounded text-xs">삭제</button>
-                        </form>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        
-        <div class="container p-5">
-            {{ $articles->links() }}        
-        </div>
+<x-app-layout>
+    <x-slot name="header">  
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            글목록
+        </h2>
+    </x-slot>
 
-    </body>
-</html>
+    <div class="container p-5">
+        @foreach($articles as $article)
+            <div class="background-white border rounded mb-3 p-3">
+                <p>{{ $article->body }}</p>
+                <p>{{ $article->user->name }}</p>
+                <p><a href="{{ route('articles.show', ['article' => $article->id]) }}">{{ $article->created_at->diffForHumans() }}</p>
+                <div class="flex flex-row">
+                    <p class="mr-1">
+                        <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="button rounded bg-blue-500 px-2 py-1 text-xs text-white">
+                            수정
+                        </a>
+                    </p>
+                    <form action="{{ route('articles.destroy', ['article' => $article->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="py-1 px-2 bg-red-500 text-white rounded text-xs">삭제</button>
+                    </form>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    
+    <div class="container p-5">
+        {{ $articles->links() }}        
+    </div>
+</x-app-layout>
