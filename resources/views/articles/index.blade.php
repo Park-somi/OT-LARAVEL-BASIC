42760276f9f6de8489a8e8f68253897ffc0541e4
Park-somi <somp8320@gmail.com>
2024-12-02 17:44:06 +0900

    [박소미] 32. 라우트 보호하기와 미들웨어

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -12,16 +12,20 @@
                 <p>{{ $article->user->name }}</p>
                 <p><a href="{{ route('articles.show', ['article' => $article->id]) }}">{{ $article->created_at->diffForHumans() }}</p>
                 <div class="flex flex-row">
+                    @can('update', $article)
                     <p class="mr-1">
                         <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="button rounded bg-blue-500 px-2 py-1 text-xs text-white">
                             수정
                         </a>
                     </p>
+                    @endcan
+                    @can('delete', $article)
                     <form action="{{ route('articles.destroy', ['article' => $article->id]) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button class="py-1 px-2 bg-red-500 text-white rounded text-xs">삭제</button>
                     </form>
+                    @endcan
                 </div>
             </div>
         @endforeach
