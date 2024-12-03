17653f05d2675fc55d851349caa99ad33c6bc809
Park-somi <somp8320@gmail.com>
2024-12-03 13:57:48 +0900

    [박소미] 38. 댓글 수 보여주기

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,7 +1,14 @@
 <x-app-layout>
     <div class="container p-5 mx-auto">
         <div class="border rounded p-4">
-            {{ $article->body }}
+            <p>{{ $article->body }}</p>
+            <p>{{ $article->user->name }}</p>
+            <p class="text-xs text-gray-500">
+                <a href="{{ route('articles.show', ['article' => $article->id]) }}">
+                    {{ $article->created_at->diffForHumans() }}
+                    <span>댓글 {{ $article->comments_count }}</span>
+                </a>
+            </p>
     
             <x-article-button-group :article=$article/>
         </div>
@@ -20,6 +27,14 @@
             <!-- 댓글 작성 폼 끝 -->
 
             <!-- 댓글 목록 시작 -->
+            <div class="mt-4 flex flex-col space-y-4">
+                @foreach($article->comments as $comment)
+                    <div>
+                        <p>{{ $comment->body }}</p>
+                        <p class="text-sm text-gray-500">{{ $comment->user->name }} {{ $article->created_at->diffForHumans() }}</p>
+                    </div>
+                @endforeach
+             </div>
             <!-- 댓글 목록 끝 -->
         </div>
         <!-- 댓글 영역 끝 -->
