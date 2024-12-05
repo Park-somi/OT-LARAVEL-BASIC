39d052da1d348689ad09076748955ec08d743631
Park-somi <somp8320@gmail.com>
2024-12-05 13:43:27 +0900

    [박소미] 프로필 페이지 - username 오류 수정

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,11 +7,18 @@
                     게시글 {{ $user->articles->count() }}
                     구독자 {{ $user->followers->count() }}
                 </div>
-                <!-- 로그인한 사용자와 프로필 주인과 다를 때 버튼 생성 -->
-                @if(Auth::id() != $user->id)
+                <!--
+                    로그인되어있고, 
+                    로그인한 사용자와 프로필 주인과 다른 경우 버튼 생성 
+                -->
+                @if(Auth::check() && Auth::id() != $user->id)
                     <div>
-                        <!-- 팔로워하고 있다면 -->
-                        @if(Auth::user()->isFollowing($user))
+                        <!-- 로그인되어있고, 팔로워하고 있다면 -->
+                        <!-- 
+                            로그인여부 확인 후 true면 조건문 실행
+                            false면 조건문 전체가 실행이 되지 않음
+                        -->
+                        @if(Auth::check() && Auth::user()->isFollowing($user))
                             <form method="POST" action="{{ route('unfollow', ['user' => $user->username]) }}">
                                 @csrf
                                 @method('delete')
